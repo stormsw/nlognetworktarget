866c53a39dc2fb962886bb3062e42d62f1367702
Alexander Varchenko <alexander.varchenko@gmail.com>
2013-11-29 02:58:20 +0200

    no message

--- ConsoleApplication/UdpServer/main.cpp
+++ ConsoleApplication/UdpServer/main.cpp
@@ -3,21 +3,103 @@
 
 #include "stdafx.h"
 #include "udp_server.h"
+#include <windows.h> 
+#include <stdio.h> 
+
+
+boost::asio::io_service io_service;
+
+BOOL CtrlHandler(DWORD fdwCtrlType)
+{
+	switch (fdwCtrlType)
+	{
+		// Handle the CTRL-C signal. 
+	case CTRL_C_EVENT:
+		printf("Ctrl-C event\n\n");
+		Beep(750, 300);
+		io_service.stop();
+		return(TRUE);
+
+		// CTRL-CLOSE: confirm that the user wants to exit. 
+	case CTRL_CLOSE_EVENT:
+		Beep(600, 200);
+		printf("Ctrl-Close event\n\n");
+		io_service.stop();
+		return(TRUE);
+
+		// Pass other signals to the next handler. 
+	case CTRL_BREAK_EVENT:
+		Beep(900, 200);
+		printf("Ctrl-Break event\n\n");
+		io_service.stop();
+		return FALSE;
+
+	case CTRL_LOGOFF_EVENT:
+		Beep(1000, 200);
+		printf("Ctrl-Logoff event\n\n");
+		io_service.stop();
+		return FALSE;
+
+	case CTRL_SHUTDOWN_EVENT:
+		Beep(750, 500);
+		printf("Ctrl-Shutdown event\n\n");
+		io_service.stop();
+		return FALSE;
+
+	default:
+		return FALSE;
+	}
+	/*
+	boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
+	signals.async_wait(
+		boost::bind(&boost::asio::io_service::stop, &io_service));//*/
+}
+
+//[ example_tutorial_file_advanced
+//void init()
+//{
+//	logging::add_file_log
+//		(
+//		keywords::file_name = "sample_%N.log",                                        /*< file name pattern >*/
+//		keywords::rotation_size = 10 * 1024 * 1024,                                   /*< rotate files every 10 MiB... >*/
+//		keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0), /*< ...or at midnight >*/
+//		keywords::format = "[%TimeStamp%]: %Message%"                                 /*< log record format >*/
+//		);
+//
+//	logging::core::get()->set_filter
+//		(
+//		logging::trivial::severity >= logging::trivial::info
+//		);
+//}
+//]
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
-  try
-  {
-    boost::asio::io_service io_service;
-    udp_server server(io_service);
-    io_service.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
 
+	if (SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE))
+	{
+		printf("\nThe Control Handler is installed.\n");
+		printf("\n -- Now try pressing Ctrl+C or Ctrl+Break, or");
+		printf("\n    try logging off or closing the console...\n");
+		printf("\n(...waiting in a loop for events...)\n\n");
+
+		try
+		{
+			udp_server server(io_service);
+			io_service.run();
+		}
+		catch (std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	else
+	{
+		printf("\nERROR: Could not set control handler");
+		return 1;
+
+	}
 	return 0;
 }
 
--- ConsoleApplication/UdpServer/udp_server.cpp
+++ ConsoleApplication/UdpServer/udp_server.cpp
@@ -1,11 +1,64 @@
 #include "stdafx.h"
 #include "udp_server.h"
+// logging set
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 
 #define SIZE 26
 #define PORT 9999
 
+
 using boost::asio::ip::udp;
 
+namespace logging = boost::log;
+namespace sinks = boost::log::sinks;
+namespace src = boost::log::sources;
+namespace expr = boost::log::expressions;
+namespace attrs = boost::log::attributes;
+namespace keywords = boost::log::keywords;
+
+
+void log(char * buf)
+{
+	//BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+	//BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+	//BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+	//BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+	//BOOST_LOG_TRIVIAL(error) << "An error severity message";
+	//BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+
+	// Filtering can also be applied
+	//using namespace boost::log;
+	
+	//logging::add_file_log
+	//	(
+	//	keywords::file_name = "sample_%N.log", 1
+	//	keywords::rotation_size = 10 * 1024 * 1024, 2
+	//	keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0), 3
+	//	keywords::format = "[%TimeStamp%]: %Message%"                                 4
+	//	);
+
+	logging::core::get()->set_filter
+		(
+		logging::trivial::severity >= logging::trivial::info
+		);
+
+	// Now the first two lines will not pass the filter
+	//BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+	//BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+	//BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+	//BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+	//BOOST_LOG_TRIVIAL(error) << "An error severity message";
+	//BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+
+	BOOST_LOG_TRIVIAL(info) << buf;
+}
 	std::string make_daytime_string()
 	{
 		using namespace std; // For time_t, time and ctime;
@@ -17,7 +70,7 @@ using boost::asio::ip::udp;
 		time( &ltime );
 		err = ctime_s( buf, SIZE, &ltime );
 		if (err != 0)
-		{
+		{			
 			printf("Invalid Arguments for _wctime_s. Error Code: %d\n", err);
 		}
 		return buf;
@@ -58,6 +111,7 @@ using boost::asio::ip::udp;
 	  memcpy_s(buf1,4096,recv_buffer_.data(),bytes_transferred);
 	  buf1[bytes_transferred]=0;
 	  printf(buf1);
+	  log(buf1);
       start_receive();
     }
   }
