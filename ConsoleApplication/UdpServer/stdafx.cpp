4e2bf8ee7005bebc43de6ac28d452be90072a3fc
Alexander Varchenko <alexander.varchenko@gmail.com>
2014-02-12 20:35:01 +0200

    Added resource

--- ConsoleApplication/UdpServer/stdafx.cpp
+++ ConsoleApplication/UdpServer/stdafx.cpp
@@ -1,6 +1,9 @@
 // stdafx.cpp : source file that includes just the standard includes
 // ConsoleApplication1.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
+#if USING_V110_SDK71 == 1 
+	#define USING_V110_SDK71 1 
+#endif 
 
 #include "stdafx.h"
 
